9f8339be83e1ca3cf7e25aef2afa9a08394d437d
Nathan Ngau <Tur44631@temple.edu>
2026-03-18 19:09:48 -0400

    fixed the spacing between the text boxes and the input titles

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,4 +1,4 @@
-import { Text, TextInput, TextInputProps, View } from 'react-native';
+import { StyleSheet, Text, TextInput, TextInputProps, View } from 'react-native';
 import tokens from '../theme/tokens';
 
 type InputProps = {
@@ -29,7 +29,11 @@ export default function Input({
   return (
     <View>
       {label ? (
-        <Text className="mb-2 text-label text-text-muted" numberOfLines={1}>
+        <Text
+          className="text-text-muted"
+          style={styles.labelText}
+          numberOfLines={1}
+        >
           {label}
         </Text>
       ) : null}
@@ -45,8 +49,20 @@ export default function Input({
         textContentType={textContentType}
         editable={editable}
         className="rounded-input border border-input-border bg-input-bg px-4 text-text-primary"
-        style={{ height: tokens.sizes.inputHeight }}
+        style={styles.textInput}
       />
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  labelText: {
+    fontSize: 16,
+    lineHeight: 20,
+    marginBottom: 6,
+  },
+  textInput: {
+    height: tokens.sizes.inputHeight - 4,
+    marginTop: 0,
+  },
+});
